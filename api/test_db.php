1609d09194d02a85810346911f60834e494f4b7b
Akibuddin2pronto <mohd.akibuddin@prontonetworks.ai>
2026-02-08 23:02:54 +0530

    updated test db file

--- api/test_db.php
+++ api/test_db.php
@@ -1,7 +1,13 @@
 <?php
-$conn = new mysqli("localhost","akibwork_expuser","PASSWORD","akibwork_expense");
+$conn = new mysqli(
+  "localhost",
+  "akibwork_expuser",
+  "Saniya1@asd",
+  "akibwork_expense"
+);
 
 if ($conn->connect_error) {
-  die("FAILED");
+  die("FAILED: " . $conn->connect_error);
 }
-echo "CONNECTED";
+
+echo "CONNECTED SUCCESSFULLY";
